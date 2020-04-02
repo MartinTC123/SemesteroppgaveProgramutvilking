87738de0c16a5bac7f5785c23d5ab620d128c48e
Martin <martin_candor@hotmail.com>
2020-04-02 22:11:16 +0200

    hei

--- src/Datamaskin/Komponenter/Tastatur.java
+++
@@ -1,9 +0,0 @@
-package Datamaskin.Komponenter;
-
-import Datamaskin.Komponent;
-
-public class Tastatur extends Komponent {
-    public Tastatur(String navn, int pris){
-        super(navn, pris);
-    }
-}
