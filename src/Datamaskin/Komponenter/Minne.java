3e2c3a32f608ada02c170ffc5366b341cdb10e51
eliasskoglund <eliasidrupsen@gmail.com>
2020-04-02 20:58:48 +0200

    asd

--- src/Datamaskin/Komponenter/Minne.java
+++ src/Datamaskin/Komponenter/Minne.java
@@ -2,12 +2,14 @@ package Datamaskin.Komponenter;
 
 import Datamaskin.Komponent;
 
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Minne extends Komponent {
 
-    private int RAM;
+    private SimpleIntegerProperty RAM;
 
     public Minne(String navn, int pris, int RAM){
         super(navn, pris);
-        this.RAM= RAM;
+        this.RAM= new SimpleIntegerProperty(RAM);
     }
 }
