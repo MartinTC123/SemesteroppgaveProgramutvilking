bf01b806b19fbba00aa15b007e505721ad072da2
kykauwi <themagnus1208@gmail.com>
2020-04-02 20:14:38 +0200

    gjort fxml på Sluttbruker

--- src/Sluttbruker/Sluttbruker.java
+++ src/Sluttbruker/Sluttbruker.java
@@ -1,4 +1,30 @@
 package Sluttbruker;
 
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
 public class Sluttbruker {
-}
+
+        @FXML
+        private TextField innFornavn;
+
+        @FXML
+        private TextField innEtternavn;
+
+        @FXML
+        public void leggTilHandlekurv(ActionEvent event) {
+
+        }
+
+        @FXML
+        public void seHandlekurv(ActionEvent event) {
+
+        }
+
+    }
+
+
