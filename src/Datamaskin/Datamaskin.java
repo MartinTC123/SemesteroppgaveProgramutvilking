6c4bd88193de6f647b7703d3fcb749464bcdfffb
eliasskoglund <eliasidrupsen@gmail.com>
2020-04-05 15:53:09 +0200

    asd

--- src/Datamaskin/Datamaskin.java
+++ src/Datamaskin/Datamaskin.java
@@ -1,4 +1,13 @@
 package Datamaskin;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+
 public class Datamaskin {
+    private ObservableList<Komponent> Datamaskin = FXCollections.observableArrayList();
+
+    public void FestTableView(TableView tv) {
+        tv.setItems(Datamaskin);
+    }
 }
