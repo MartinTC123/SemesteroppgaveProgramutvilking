f19cf31ac225681c3cb20cb0ffa4201ff5dedc7a
kykauwi <themagnus1208@gmail.com>
2020-04-05 15:45:40 +0200

    gjort fxml på Superbruker

--- src/Superbruker/Superbruker.java
+++ src/Superbruker/Superbruker.java
@@ -1,4 +1,28 @@
 package Superbruker;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Superbruker {
+
+    @FXML
+    private TextField innKomponent;
+
+    @FXML
+    private TextField innNavn;
+
+    @FXML
+    private TextField innPris;
+
+    @FXML
+    void lagreEndringer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void leggTilKomponent(ActionEvent event) {
+
+    }
+
 }
