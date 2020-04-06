a4483225d31a2a3d2435452fda77104e9875b6a0
Martin <martin_candor@hotmail.com>
2020-04-06 15:13:33 +0200

    hei

--- src/Sluttbruker/Sluttbruker.java
+++ src/Sluttbruker/Sluttbruker.java
@@ -1,14 +1,17 @@
 package Sluttbruker;
 
-
-
+import Datamaskin.Komponent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-
-public class Sluttbruker {
+public class Sluttbruker implements Initializable {
 
         @FXML
         private TextField innFornavn;
@@ -17,14 +20,42 @@ public class Sluttbruker {
         private TextField innEtternavn;
 
         @FXML
-        public void leggTilHandlekurv(ActionEvent event) {
+        private TableView<Komponent> table1;
+
+        @FXML
+        private TableView<Komponent> table2;
+
+        @FXML
+        private TableColumn<Komponent, String> col1Navn;
+
+        @FXML
+        private TableColumn<Komponent, String> col1Komponent;
+
+        @FXML
+        private TableColumn<Komponent, Integer> col1Pris;
+
+        @FXML
+        private TableColumn<Komponent, String> col2Navn;
+
+        @FXML
+        private TableColumn<Komponent, String> col2Komponent;
+
+        @FXML
+        private TableColumn<Komponent, Integer> col2Pris;
+
+        @Override
+        public void initialize(URL location, ResourceBundle resources) {
 
         }
 
         @FXML
-        public void seHandlekurv(ActionEvent event) {
+        void leggTilHandlekurv(ActionEvent event) {
 
         }
+
+        @FXML
+        void seHandlekurv(ActionEvent event) {
+
+        }
+
 }
-
-
