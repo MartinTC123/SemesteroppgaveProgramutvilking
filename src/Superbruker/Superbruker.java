ad8780477e3e4586316c2caf15a56336d53da2df
Martin <martin_candor@hotmail.com>
2020-04-08 23:23:29 +0200

    hei

--- src/Superbruker/Superbruker.java
+++ src/Superbruker/Superbruker.java
@@ -2,12 +2,12 @@ package Superbruker;
 
 import Datamaskin.Komponent;
 import Datamaskin.KomponentCollection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.converter.IntegerStringConverter;
 
@@ -16,14 +16,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Superbruker implements Initializable {
-
-    KomponentCollection kColl3= new KomponentCollection();
-
-    ArrayList<Komponent> dataListe= new ArrayList<>();
-
-    Komponent test= new Komponent("Test1", "Minne", 1000);
-    Komponent test1= new Komponent("Test2", "Skjermkort", 1600);
-    Komponent test2= new Komponent("Test3", "Harddisk", 2000);
 
     @FXML
     private TableView tabell3;
@@ -38,7 +30,7 @@ public class Superbruker implements Initializable {
     private TableColumn<Komponent, Integer> prisC3;
 
     @FXML
-    private TextField innKomponent;
+    private ChoiceBox<String> choiceBox;
 
     @FXML
     private TextField innNavn;
@@ -50,19 +42,44 @@ public class Superbruker implements Initializable {
     private TextField txtFiltrer;
 
     @FXML
+    private Label lblNyttKomponent;
+
+    @FXML
     void lagreEndringer(ActionEvent event) {
 
     }
 
     @FXML
     void leggTilKomponent(ActionEvent event) {
-
+        Komponent nyttKomponent= null;
+        try {
+            nyttKomponent = opprettKomponent();
+        }
+        catch (NumberFormatException e){
+            lblNyttKomponent.setText(e.getMessage());
+        }
+        if (nyttKomponent != null){
+            kColl3.leggTilElement(nyttKomponent);
+            resetTextFields();
+        }
     }
+
+    KomponentCollection kColl3= new KomponentCollection();
+
+    ArrayList<Komponent> dataListe= new ArrayList<>();
+
+    ObservableList<String> tilgjengeligeValg= FXCollections.observableArrayList("Prosessor", "Skjermkort", "Minne", "Harddisk", "Tastatur", "Mus", "Skjerm");
+
+    Komponent test= new Komponent("Test1", "Minne", 1000);
+    Komponent test1= new Komponent("Test2", "Skjermkort", 1600);
+    Komponent test2= new Komponent("Test3", "Harddisk", 2000);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         kColl3.kobleTilTableView(tabell3);
         kColl3.sorterTableView(tabell3, txtFiltrer);
+
+        choiceBox.setItems(tilgjengeligeValg);
 
         navnC3.setCellFactory(TextFieldTableCell.forTableColumn());
         komponentC3.setCellFactory(TextFieldTableCell.forTableColumn());
@@ -73,6 +90,22 @@ public class Superbruker implements Initializable {
         kColl3.leggTilElement(test2);
 
         tabell3.setEditable(true);
+    }
+
+    private Komponent opprettKomponent(){
+        String navn= innNavn.getText();
+        String komponent= choiceBox.getSelectionModel().getSelectedItem();
+        int pris= Integer.parseInt(innPris.getText());
+
+        Komponent nyttKomponent= new Komponent(navn, komponent, pris);
+
+        return nyttKomponent;
+    }
+
+    private void resetTextFields(){
+        innNavn.setText("");
+        innPris.setText("");
+        choiceBox.getSelectionModel().clearSelection();
     }
 
     @FXML
